b57e68d3ad76b904db24409b989cea171cee1027
mdjasim786 <mdjasimcclgrd12a@gmail.com>
2019-09-30 19:41:28 +0530

    updated

--- asyncauto.js
+++ asyncauto.js
@@ -1,26 +1,21 @@
 var async = require('async');
 
 async.auto({
-    intern_name: function (callback) {
+    intern_name: callback=> {
         var name = "Jasim";
         callback(null, name);
     },
-    intern_work: function (callback) {
+    intern_work: callback=>{
         var work = 'Backend';
         callback(null, work);
     },
-    getDetail: ['intern_name', 'intern_work', function (result, callback) {
+    getDetail: ['intern_name', 'intern_work', (result, callback)=> {
         var work_experience = "6months"
         var job_grade = "B";
         var intern_profile= `The Intern name is ${result.intern_name}  work as a ${result.intern_work} and work experience of ${work_experience}and his job grade is ${job_grade}`;
         callback(null, intern_profile);
     }],
 
-}, function (err, results) {
-    if(results){
+}, (err, results)=> {
         console.log(results);
-    }else{
-        console.log(error);
-    }
-
 });
--- asyncwaterfall.js
+++ asyncwaterfall.js
@@ -1,23 +1,23 @@
 var async = require('async');
   async.waterfall([
-    function(callback) {
+    callback=> {
         var num1 = 10;
         var num2 =20;
         console.log(`Number1 is ${num1} and Number2 is ${num2}`);
         
       callback(null, num1, num2);
     },
-    function(x, y, callback) {
+    (x, y, callback)=> {
       let sum = x + y;
       console.log(`Sum is : ${sum}`);
       
       callback(null, sum);
     },
-    function(sum, callback) {
+    (sum, callback)=> {
       let avg = sum/2;
       callback(null, avg);
     }
-  ], function(err, result) {
+  ], (err, result)=> {
     console.log(`Average is : ${result}`);
   });   
 
